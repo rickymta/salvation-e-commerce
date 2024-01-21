45d63e8105dcd97accdcbb65a64d1ca656acff2e
QuanDH <rickymta@gmail.com>
2024-01-21 12:43:03 +0700

    Add test project and refactor unnecessary path in csproj file

---
+++ test/Salvation.Library.Services.Test/AccountServiceTest.cs
@@ -0,0 +1,16 @@
+namespace Salvation.Library.Services.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ test/Salvation.Service.WebAPI.Test/AccountControllerTest.cs
@@ -0,0 +1,16 @@
+namespace Salvation.Service.WebAPI.Test
+{
+    public class AccountControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
